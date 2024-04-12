fb81901765e450a3c700addee8de6ef873a35a8a
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 <picard.boris@gmail.com>
2024-04-12 11:33:49 +0200

    init project IOT

---
+++ config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+//Gestion des erreurs
+$errors = [];
+
+// Connexion a la bdd
+define('DSN', 'mysql:host=localhost;dbname=iot');
+define('USER', 'BorisIot');
+define('PASSWORD', 'FEUIgsqI2QvdK@P)');
