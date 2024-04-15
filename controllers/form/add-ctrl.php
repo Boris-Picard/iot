dc46bf2b9e7c67af885a430e9764fdf935cce2a4
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 <picard.boris@gmail.com>
2024-04-15 14:25:24 +0200

    ajout is-invalid or is-valid

--- controllers/form/add-ctrl.php
+++ controllers/form/add-ctrl.php
@@ -7,84 +7,93 @@ require_once __DIR__ . '/../../config/config.php';
 
 $addPage = true;
 try {
+    $valid = [];
 
     $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_SPECIAL_CHARS);
 
-    if (empty($name)) {
-        $errors['name'] = 'Veuillez rentrer un nom';
-    } else {
-        $isOk = filter_var($name, FILTER_DEFAULT);
-        if (!$isOk) {
-            $errors['name'] = 'Nom invalide';
-        }
-    }
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-    $description = filter_input(INPUT_POST, 'description', FILTER_SANITIZE_SPECIAL_CHARS);
-
-    if (empty($description)) {
-        $errors['description'] = 'Veuillez rentrer une description';
-    } else {
-        $isOk = filter_var($description, FILTER_DEFAULT);
-        if (!$isOk) {
-            $errors['description'] = 'Description invalide';
-        }
-        if (strlen($description) > 500) {
-            $errors['description'] = 'Description trop longue pas plus de 500 caractères';
-        }
-        if (strlen($description) < 5) {
-            $errors['description'] = 'Description trop courte pas moins de 5 caractères';
-        }
-    }
-
-    $type = filter_input(INPUT_POST, 'type', FILTER_SANITIZE_SPECIAL_CHARS);
-
-    if (empty($type)) {
-        $errors['type'] = 'Veuillez rentrer un type';
-    } else {
-        $isOk = filter_var($type, FILTER_DEFAULT);
-        if (!$isOk) {
-            $errors['type'] = 'Type invalide';
-        }
-    }
-
-    if (empty($errors)) {
-        try {
-            $pdo = Database::connect();
-            $pdo->beginTransaction();
-
-            $module = new Module();
-
-            $module->setName($name);
-            $module->setDescription($description);
-            $module->setMeasurementType($type);
-
-            $resultModule = $module->insert();
-
-            $id_modules = $pdo->lastInsertId();
-
-            $moduleData = new ModuleData();
-
-            $moduleData->setModuleValue(rand(1, 100));
-            $moduleData->setIdModules($id_modules);
-
-            $data = $moduleData->insert();
-
-            $moduleStatus = new ModuleStatus();
-
-            $moduleStatus->setIsOperational(true);
-            $moduleStatus->setDuration(0);
-            $moduleStatus->setDataCount(0);
-            $moduleStatus->setIdModules($id_modules);
-
-            $status = $moduleStatus->insert();
-
-            $pdo->commit();
-
-            if ($resultModule || $data || $status) {
-                $alert['success'] = 'La donnée a bien été ajouté !';
+        if (empty($name)) {
+            $errors['name'] = 'Veuillez rentrer un nom';
+        } else {
+            $isOk = filter_var($name, FILTER_DEFAULT);
+            $valid['name'] = true;
+            if (!$isOk) {
+                $errors['name'] = 'Nom invalide';
             }
-        } catch (PDOException $e) {
-            $pdo->rollback();
+        }
+
+        $description = filter_input(INPUT_POST, 'description', FILTER_SANITIZE_SPECIAL_CHARS);
+
+        if (empty($description)) {
+            $errors['description'] = 'Veuillez rentrer une description';
+        } else {
+            $isOk = filter_var($description, FILTER_DEFAULT);
+            $valid['description'] = true;
+            if (!$isOk) {
+                $errors['description'] = 'Description invalide';
+            }
+            if (strlen($description) > 500) {
+                $errors['description'] = 'Description trop longue pas plus de 500 caractères';
+            }
+            if (strlen($description) < 5) {
+                $errors['description'] = 'Description trop courte pas moins de 5 caractères';
+            }
+        }
+
+        $type = filter_input(INPUT_POST, 'type', FILTER_SANITIZE_SPECIAL_CHARS);
+
+        if (empty($type)) {
+            $errors['type'] = 'Veuillez rentrer un type';
+        } else {
+            $isOk = filter_var($type, FILTER_DEFAULT);
+            $valid['type'] = true;
+            if (!$isOk) {
+                $errors['type'] = 'Type invalide';
+            }
+        }
+
+        if (empty($errors)) {
+            try {
+                $pdo = Database::connect();
+                $pdo->beginTransaction();
+
+                $module = new Module();
+
+                $module->setName($name);
+                $module->setDescription($description);
+                $module->setMeasurementType($type);
+
+                $resultModule = $module->insert();
+
+                $id_modules = $pdo->lastInsertId();
+
+                $moduleData = new ModuleData();
+
+                $randomInt = rand(10, 1000);
+                $randomDec = $randomInt / 100;
+                $moduleData->setModuleValue($randomDec);
+                $moduleData->setIdModules($id_modules);
+
+                $data = $moduleData->insert();
+
+                $moduleStatus = new ModuleStatus();
+
+                $moduleStatus->setIsOperational(true);
+                $moduleStatus->setDuration(0);
+                $moduleStatus->setDataCount(0);
+                $moduleStatus->setIdModules($id_modules);
+
+                $status = $moduleStatus->insert();
+
+                $pdo->commit();
+
+                if ($resultModule || $data || $status) {
+                    $alert['success'] = 'La donnée a bien été ajouté !';
+                }
+            } catch (PDOException $e) {
+                $pdo->rollback();
+            }
         }
     }
 } catch (PDOException $e) {
--- views/form/add.php
+++ views/form/add.php
@@ -11,17 +11,17 @@
                     Ajouter un nouveau module
                 </h1>
                 <div class="form-floating mb-4">
-                    <input type="text" class="form-control <?= isset($errors['name']) ? "is-invalid" : "" ?>" name="name" id="name" placeholder="Ex : Capteur de température" value="<?= $name ?? "" ?>" required>
+                    <input type="text" class="form-control <?= !empty($errors['name']) ? "is-invalid" : "" ?> <?= isset($valid['name']) ? "is-valid" : "" ?>" name="name" id="name" placeholder="Ex : Capteur de température" value="<?= $name ?? "" ?>" required>
                     <label for="name">Nom<span class="text-danger">*</span></label>
                     <small class="text-danger mx-1"><?= $errors['name'] ?? '' ?></small>
                 </div>
                 <div class="form-floating mb-4">
-                    <textarea type="text" class="form-control <?= isset($errors['description']) ? "is-invalid" : "" ?>" name="description" id="description" placeholder="Capteur de température industriel" required><?= $description ?? "" ?></textarea>
+                    <textarea type="text" class="form-control <?= !empty($errors['description']) ? "is-invalid" : "" ?> <?= isset($valid['description']) ? "is-valid" : "" ?>" name="description" id="description" placeholder="Capteur de température industriel" required><?= $description ?? "" ?></textarea>
                     <label for="description">Description<span class="text-danger">*</span></label>
                     <small class="text-danger mx-1"><?= $errors['description'] ?? '' ?></small>
                 </div>
                 <div class="form-floating mb-4">
-                    <input type="text" class="form-control <?= isset($errors['type']) ? "is-invalid" : "" ?>" name="type" id="type" placeholder="Ex: Température" value="<?= $type ?? "" ?>" required>
+                    <input type="text" class="form-control <?= !empty($errors['type']) ? "is-invalid" : "" ?> <?= isset($valid['type']) ? "is-valid" : "" ?>" name="type" id="type" placeholder="Ex: Température" value="<?= $type ?? "" ?>" required>
                     <label for="type">Type de mesure<span class="text-danger">*</span></label>
                     <small class="text-danger mx-1"><?= $errors['type'] ?? '' ?></small>
                 </div>
