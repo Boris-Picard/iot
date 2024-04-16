d52e2d91fdc232649092ccf786c2709a8ac18eb1
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 <picard.boris@gmail.com>
2024-04-16 14:11:38 +0200

    maj du graphique toutes les secondes lorsque de nouvelles valeurs sont ajoutées

--- public/assets/js/value.js
+++ public/assets/js/value.js
@@ -1,21 +1,62 @@
 let timestamps = [];
 let values = [];
+let myChart;
+
 const value = () => {
   const urlParams = new URLSearchParams(window.location.search);
   const id = urlParams.get("id");
   fetch(`/controllers/form/ajax-value.php?id=${id}`)
-    .then((response) => {
-      return response.json();
-    })
+    .then((response) => response.json())
     .then((data) => {
+      timestamps = [];
+      values = [];
+
+      // On récupère les données nécessaires.
       data.forEach((item) => {
         timestamps.push(item.module_timestamp);
         values.push(item.module_value);
       });
+
+      // Si myChart existe déjà, nous mettons à jour ses valeurs. Sinon, nous appelons le graphique de base.
+      if (myChart) {
+        myChart.data.labels = timestamps;
+        myChart.data.datasets[0].data = values;
+        myChart.update();
+      } else {
+        chart();
+      }
     })
     .catch((error) => {
-      console.error("erreur", error);
+      console.error("Erreur", error);
     });
 };
 
-setInterval(value(), 1000);
+const chart = () => {
+  const ctx = document.getElementById("myChart");
+  myChart = new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: timestamps,
+      datasets: [
+        {
+          label: "Température",
+          data: values,
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  });
+};
+
+// On appelle value pour la mise en page de base lors du premier chargement.
+value();
+
+// Permet de mettre à jour le graphique toutes les secondes lorsque de nouvelles valeurs sont ajoutées.
+setInterval(value, 1000);
--- views/templates/footer.php
+++ views/templates/footer.php
@@ -6,37 +6,9 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
 </script>
 <?php if (isset($viewModule)) { ?>
-    <script src="/public/assets/js/value.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js@4.4.2/dist/chart.umd.min.js"></script>
-    <script>
-        const chart = () => {
-            const ctx = document.getElementById('myChart');
-            console.log(values);
-            new Chart(ctx, {
-                type: 'line',
-                data: {
-                    labels: timestamps,
-                    datasets: [{
-                        label: '# of Votes',
-                        data: values.map((val) => {
-                            return val
-                        }),
-                        borderWidth: 1
-                    }]
-                },
-                options: {
-                    scales: {
-                        y: {
-                            beginAtZero: true
-                        }
-                    }
-                }
-            });
-        }
-        chart();
-    </script>
+    <script src="/public/assets/js/value.js"></script>
 <?php } ?>
-<?php var_dump($getModule); ?>
 </body>
 
 </html>
