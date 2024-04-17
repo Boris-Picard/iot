b3ca86193881b70ffb968f98cc4614990d811ea4
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 <picard.boris@gmail.com>
2024-04-17 17:42:29 +0200

    convertion des secondes en minutes heures et jours

--- public/assets/js/setDuration.js
+++ public/assets/js/setDuration.js
@@ -6,7 +6,17 @@ const setDuration = () => {
       return response.json();
     })
     .then((data) => {
-      document.getElementById("duration").innerHTML = data[0].duration;
+      let durationInSeconds = data[0].duration;
+
+      let minutes = Math.floor(durationInSeconds / 60);
+
+      let hours = Math.floor(durationInSeconds / 3600);
+
+      let days = Math.floor(durationInSeconds / (3600 * 24));
+
+      document.getElementById("duration").innerHTML = `${days} jours, ${
+        hours % 24
+      } heures, ${minutes % 60} minutes, ${durationInSeconds % 60} secondes`;
       document.getElementById("data_count").innerHTML = data[0].data_count;
       document.getElementById("last_updated").innerHTML = data[0].updated_at;
     })
