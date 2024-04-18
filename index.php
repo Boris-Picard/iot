4bf11672cb1e777a8f4d4237fe83415c6496ee73
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 <picard.boris@gmail.com>
2024-04-18 13:00:54 +0200

    maj pages

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
-<?php
-
-header('location: /controllers/form/add-ctrl.php');
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="refresh" content="0; URL=/controllers/form/add-ctrl.php">
+    <title>Redirection</title>
+</head>
+<body>
+    <!-- Contenu de la page s'il y en a -->
+</body>
+</html>
